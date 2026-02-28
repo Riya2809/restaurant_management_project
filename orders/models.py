349aa12d58ce4e0576d8483d9264044308aa74f3
Riya2809 <riyashinde862@gmail.com>
2026-02-28 13:35:43 +0530

    66770

--- orders/models.py
+++ orders/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique = True)
+    discount_coverage = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
+
 
     def __str__(self):
-        return self.name
+        return self.code
 
 
